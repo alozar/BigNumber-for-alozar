0b999fb773db1c7ebc4276bef8dde071eddd34ae
alozar <klimovforever.92@mail.ru>
2023-08-15 18:06:48 +0800

    Предварительный вариант решения для перегрузки operator +

--- BigNumber.cs
+++ BigNumber.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace Interview
@@ -32,6 +33,8 @@ namespace Interview
         // лучше хранить в обратном порядке
         private int[] digits;
 
+        public int[] Digits {  get { return digits; } }
+
         public BigNumber(string x)
         {
             digits = ParseDigits(x);
@@ -49,7 +52,45 @@ namespace Interview
 
         public static BigNumber operator +(BigNumber a, BigNumber b)
         {
-            throw new NotImplementedException();
+            var builder = new StringBuilder("");
+            var isFinal = false;
+            var ind = 0;
+            var tempDigit = 0;
+            var curSum = 0;
+            while (!isFinal)
+            {
+                curSum = tempDigit;
+
+                if (ind < a.Digits.Length)
+                {
+                    curSum += a.Digits[ind];
+                }
+
+                if (ind < b.Digits.Length)
+                {
+                    curSum += b.Digits[ind];
+                }
+
+                ind++;
+                isFinal = ind >= a.Digits.Length && ind >= b.Digits.Length;
+
+                if (curSum > 9)
+                {
+                    var digit = curSum / 10;
+                    builder.Append(digit);
+
+                    tempDigit = curSum % 10;
+                    if (isFinal)
+                    {
+                        builder.Append(tempDigit);
+                    }
+                }
+                else
+                {
+                    builder.Append(curSum);
+                }
+            }
+            return new BigNumber(builder.ToString().Reverse().ToString());
         }
 
         private int[] ParseDigits(string number)
